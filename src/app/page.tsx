ea07b218553a4de3bddb7631562bfb0f3aa046ad
kigawa01 <contact@kigawa.net>
2024-11-08 11:33:47 +0000

    Add project configuration files and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 "use client"
-import Image from "next/image";
+import {FilmPass} from "three-stdlib";
+import {useEffect} from "react";
 
 export default function Home() {
   useEffect(() => {
-    
+    new FilmPass(0, 1, 648, false);
   }, []);
   return (
     <></>
